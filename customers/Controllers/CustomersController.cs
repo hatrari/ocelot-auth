30c6b9ffa6664976b9213404c248191a357e6911
home <ham.atrari@gmail.com>
2020-11-10 21:16:35 +0100

    add customers service

---
+++ customers/Controllers/CustomersController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace customers.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class CustomersController : ControllerBase
+  {
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "customer 1", "customer 2", "customer 3" };
+    }
+  }
+}
