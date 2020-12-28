f3a6410a0198c55d1de4f0411eab8693f3ba398e
hatrari <ham.atrari@gmail.com>
2020-12-28 12:03:55 +0100

    changing namespace

--- customers/Controllers/CustomersController.cs
+++ customers/Controllers/CustomersController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-namespace customers.Controllers
+namespace Customers.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
--- getway/Controllers/AuthController.cs
+++ getway/Controllers/AuthController.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using getway.Entities;
+using Getway.Entities;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
